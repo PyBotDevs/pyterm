1c506e2319bee633969c4b919d19e9c1c7376948
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-09-27 15:18:42 +0530

    Change `colors` to `color`

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 import os.path
 import pwd
 import time
-import corelibs.colors
+import corelibs.color
 # import datetime
 # import math
 
@@ -18,7 +18,7 @@ user = pwd.getpwuid(os.getuid())[0]
 errcode = None
 
 # Initialize Lib Modules
-colors = corelibs.colors.Stdout()
+color = corelibs.color.Stdout()
 
 
 # Core Functions
@@ -29,15 +29,15 @@ def fdate(timestamp):
 # Functions
 def start():
     if version is None or version == "":
-        print(f"==> {colors.yellow}WARNING - No version was found in app configuration{colors.end}")
-        print(f"PyTerm - Beta Release [no version] {colors.yellow}(Unstable){colors.end}")
-    else: print(f"PyTerm - Beta Release v{version} {colors.yellow}(Unstable){colors.end}")
+        print(f"==> {color.yellow}WARNING - No version was found in app configuration{color.end}")
+        print(f"PyTerm - Beta Release [no version] {color.yellow}(Unstable){color.end}")
+    else: print(f"PyTerm - Beta Release v{version} {color.yellow}(Unstable){color.end}")
     print(f"Running as PID: {pid}")
 
 
 def listen_for_command(errorcode):
-    if errorcode is None or errorcode == 0: cmd = input(f"{colors.cyan}{user}{colors.end}@{colors.blue}{host}{colors.end} <{wdir}> % ")
-    else: cmd = input(f"{colors.red}{errorcode}{colors.end} {colors.cyan}{user}{colors.end}@{colors.blue}{host}{colors.end} <{wdir}> % ")
+    if errorcode is None or errorcode == 0: cmd = input(f"{color.cyan}{user}{color.end}@{color.blue}{host}{color.end} <{wdir}> % ")
+    else: cmd = input(f"{color.red}{errorcode}{color.end} {color.cyan}{user}{color.end}@{color.blue}{host}{color.end} <{wdir}> % ")
     rescode = None
     if cmd.lower() == "exit": exit(0)
     else: rescode = os.system(cmd)
@@ -51,7 +51,7 @@ def cleanup():
     del pid
     del host
     del user
-    del colors
+    del color
 
 
 # Initialization
