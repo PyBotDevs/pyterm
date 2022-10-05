683721d3844f3da3c9917c93be14d70688975f54
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-10-05 14:27:01 +0530

    Add GUI multi-threading support for PyTerm settings window

---
+++ app/guis/settings.py
@@ -0,0 +1,13 @@
+### NKA Development Organization 2022. For enquiries, contact <pybotdevs@outlook.com> ###
+# Imports
+from tkinter import *
+from tkinter import ttk
+
+# Main Code
+def run():
+    root = Tk()
+    root.title("PyTerm Settings")
+    root.geometry("700x550")
+    root.resizable(False, False)
+
+    root.mainloop()
